e7ac3a8d137236e7526cd07f45913fc98386b65b
Adam <adamziv156@gmail.com>
2024-10-29 10:20:11 -0500

    added an app route

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 engine = create_engine('sqlite:///test.db')
 app.secret_key = "secret"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
