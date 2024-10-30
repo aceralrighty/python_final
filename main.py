a6d2c966de9b5e3c9a3243892ff17db7cdf1c36c
Adam <adamziv156@gmail.com>
2024-10-30 10:24:03 -0500

    added Enums

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-
 from models import Supply
 
 app = Flask(__name__, template_folder='templates')
 engine = create_engine('sqlite:///test.db')
 app.secret_key = "secret"
+
+
 
 
 @app.route("/")
@@ -17,12 +18,20 @@ def index():
 def add_room():
     return render_template('add_room.html')
 
+
+def is_tiling_needed(is_tiling):
+    if is_tiling:
+        return True
+    else:
+        return False
+
 @app.route('/add_supplies', methods=['GET', 'POST'])
 def add_supplies():
     if request.method == 'GET':
         session["supply_name"] = request.form["supply_name"]
         session["quantity"] = request.form["quantity"]
         session['cost_per_item'] = request.form["cost_per_item"]
+        session["is_tiling_needed"] = is_tiling_needed(request.form["is_tiling_needed"])
 
         ss = Session()
         new_supply = Supply(name=session["supply_name"], quantity=session["quantity"], cost_per_item=session["cost_per_item"])
